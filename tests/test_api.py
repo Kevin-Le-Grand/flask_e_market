b7af3d216002799d7d44fc22f8c5c8c79908e084
Kevin-Le-Grand <kev.le.grand@orange.fr>
2026-09-25 17:29:46 +0200

    ajout de token dans le test :sweat_smile:

--- tests/test_api.py
+++ tests/test_api.py
@@ -84,11 +84,13 @@ def test_recuperation_de_produits_par_nom_ou_description(client, app):
         db.session.add(product)
         db.session.commit()
 
-    response = client.get("/api/produits", query_string={"nom": "Clavier"})
+    token = connecter_utilisateur(client)
+    
+    response = client.get("/api/produits", query_string={"nom": "Clavier"}, headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 200
     assert len(response.get_json()) == 1
 
-    response = client.get("/api/produits", query_string={"description": "mécanique"})
+    response = client.get("/api/produits", query_string={"description": "mécanique"}, headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 200
     assert len(response.get_json()) == 1
 
