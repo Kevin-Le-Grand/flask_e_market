ac6a8ac68c7c550fc43bc6fc8c12143d8c7abe5e
Kevin-Le-Grand <kev.le.grand@orange.fr>
2026-09-25 18:51:00 +0200

    ajout des test dans le notebook

--- app/models.py
+++ app/models.py
@@ -88,9 +88,13 @@ class OrderItem(db.Model):
     __tablename__ = "order_item"
 
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey("order.id"), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+    order_id = db.Column(
+        "commande_id", db.Integer, db.ForeignKey("order.id"), nullable=False
+    )
+    product_id = db.Column(
+        "produit_id", db.Integer, db.ForeignKey("product.id"), nullable=False
+    )
+    quantity = db.Column("quantite", db.Integer, nullable=False)
     prix_unitaire = db.Column(db.Float, nullable=False)
     order = db.relationship("Order", back_populates="items")
     product = db.relationship("Product", back_populates="order_items")
